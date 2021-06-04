b4c54cd6d121bbd1a1efaf3cc8e70a6bdb75389a
Peter Benjamin Mhina <peterbenjaminmhina@Peters-MacBook-Pro.local>
2021-06-03 20:47:27 -0700

    wip

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 require('dotenv').config()
 const express = require('express')
 const server = express()
+const path = require('path')
+const cors = require('cors')
+
+server.use(express.json())
+server.use(cors())
+server.use(express.static(path.join(__dirname,'mywork/build')))// static assets
 
 console.log(process.env.USER)
 console.log(process.env.SHELL)
@@ -13,6 +19,10 @@ if (process.env.NODE_ENV === 'production'){
 const PORT =process.env.PORT || 5000
 
 console.log('Port is ', PORT)
+
+server.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'mywork/build','index.html'))
+})
 
 server.get('/api',(req,res)=>{
     res.json({message : `${process.env.COHORT}`})
